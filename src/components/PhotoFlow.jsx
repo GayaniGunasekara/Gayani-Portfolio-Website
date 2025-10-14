911e38341a83294329f0df814fa2b7b5d19e2611
GayaniGunasekara <gayanigunasekara2001@gmail.com>
2025-10-14 23:45:16 +0530

    Add more Photos for PhotFlow

--- src/components/PhotoFlow.jsx
+++ src/components/PhotoFlow.jsx
@@ -12,8 +12,13 @@ import img7 from "../assets/img7.jpg";
 import img8 from "../assets/img8.jpg";
 import img9 from "../assets/img9.jpg";
 import img10 from "../assets/img10.jpg";
+import img11 from "../assets/img11.jpg";
+import img12 from "../assets/img12.jpg";
+import img13 from "../assets/img13.jpg";
+import img14 from "../assets/img14.jpg";
+import img15 from "../assets/img15.jpg";
 
-const photos = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
+const photos = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15];
 
 const PhotoFlow = () => {
     const [current, setCurrent] = useState(0);
